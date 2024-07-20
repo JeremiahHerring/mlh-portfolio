3ef19cd57faa9195b4551ad1113a22f6852e55dc
Jeremiah <jeremherring@gmail.com>
2024-07-19 22:04:08 -0600

    add test cases

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                         user=os.getenv("MYSQL_USER"),
+                         password=os.getenv("MYSQL_PASSWORD"),
+                         host=os.getenv("MYSQL_HOST"),
+                         port=3306
+                         )
+
 # Retrieve database configuration from environment variables
 db_name = os.getenv("MYSQL_DATABASE")
 db_user = os.getenv("MYSQL_USER")
