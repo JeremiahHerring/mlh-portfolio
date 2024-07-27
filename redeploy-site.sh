b1b404274a33821e5a56bab832581343625e85fc
Jeremiah <jeremherring@gmail.com>
2024-07-27 17:08:55 -0600

    update redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -8,15 +8,8 @@ cd /root/mlh-portfolio || { echo "Failed to navigate to project directory"; exit
 echo "Fetching the latest changes from the main branch..."
 git fetch && git reset origin/main --hard || { echo "Failed to fetch and reset git repository"; exit 1; }
 
-# Activate the Python virtual environment and install dependencies
-echo "Activating the Python virtual environment..."
-source /root/mlh-portfolio/python3-virtualenv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+# Spin the containers down to prevent out of memor issues on our VPS instances
+docker compose -f docker-compose.yml down
 
-echo "Installing Python dependencies..."
-pip install -r requirements.txt || { echo "Failed to install Python dependencies"; exit 1; }
-
-# Restart myportfolio service
-echo "Restarting myportfolio service..."
-sudo systemctl restart myportfolio.service || { echo "Failed to restart myportfolio service"; exit 1; }
-
-echo "Deployment completed successfully."
+# Spin the container up
+docker compose -f docker-compose.yml up -d --build
